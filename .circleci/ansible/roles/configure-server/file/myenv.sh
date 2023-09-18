65a6134907bddf50fc093c2b9a6013634eda01ae
Shine <26Shiine@gmail.com>
2023-09-18 22:45:47 +0700

    triggger deploy

--- .circleci/ansible/roles/configure-server/file/myenv.sh
+++ .circleci/ansible/roles/configure-server/file/myenv.sh
@@ -1,7 +1,7 @@
 export TYPEORM_CONNECTION=postgres
 export TYPEORM_DATABASE=postgres
 export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
-export TYPEORM_HOST=udacity-project3.chwwkzlrnh0h.us-east-1.rds.amazonaws.com
+export TYPEORM_HOST=postgres.czvptwd6i6r4.us-east-1.rds.amazonaws.com
 export TYPEORM_MIGRATIONS=./src/migrations/*.ts
 export TYPEORM_MIGRATIONS_DIR=./src/migrations
 export TYPEORM_USERNAME=postgres
